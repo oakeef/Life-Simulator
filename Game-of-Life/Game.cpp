cc84970a5e12266c9fa740acf3a09e7ffdc938bb
Evan <oakeef@gmail.com>
2016-12-07 13:41:35 -0400

    Added some comments, Tried to add Move()
    Move() still doesn't work yet

--- Game-of-Life/Game.cpp
+++ Game-of-Life/Game.cpp
@@ -13,30 +13,62 @@ Game::Game() {
 void Game::run() {
 	print();
 
+
+	//one turn
+	for (int row = 0; row < GRIDSIZE; row++) {
+		for (int col = 0; col < GRIDSIZE; col++) {
+			city->getOrganism(row, col)->move();
+		}
+	}
+
+	print();
+
 }
 
 void Game::print() {
+
 	for (int row = 0; row < GRIDSIZE; row++) {
+
+		//cout a | char to greate one side of grid
 		std::cout << "|";
+
 		for (int col = 0; col < GRIDSIZE; col++) {
+
+			//checks if each "space" in the grid is a Zombie or Human
+			//Create Orangism pointer called space that gets the Organism at the current x,y location
 			Organism* space = city->getOrganism(row, col);
+
+			//iff the space is not null do this stuff
 			if (space) {
+
+				//if the space can be cast to a Zombie then print out the Zombie Char
 				if (dynamic_cast<Zombie*>(space)) {
 					std::cout << ZOMBIE_CH;
-				}
+				}//end if
+
+				//else if the space can be cast as a Human then print out the Human Char
 				else if (dynamic_cast<Human*>(space)) {
 					std::cout << HUMAN_CH;
-				}
+				}//end else if
+				
+				//in the weird circumstance where it isn't either than it becomes MISSINGNO
 				else {
 					std::cout << "?";
-				}
+				}//end else
 
-			}
+			}//end if
+
+			//if the space is a null space then the Space Char is printed out
 			else {
 				std::cout << SPACE_CH;
-			}
+			}//end else
+
+			//and finally another | to finish off the grid
 			std::cout << "|";
-		}
+
+		}//end for
+
+		//endline
 		std::cout << std::endl;
 	}
 }
--- Game-of-Life/Organism.cpp
+++ Game-of-Life/Organism.cpp
@@ -1,4 +1,5 @@
 #include "Organism.h"
+#include "City.h"
 
 Organism::Organism(City* city, int x, int y) {
 	_city = city;
@@ -8,4 +9,23 @@ Organism::Organism(City* city, int x, int y) {
 
 Organism::~Organism() {
 
+}
+
+void Organism::move() {
+	if (_city->getOrganism( (_x + 1) , _y ) == nullptr) {
+
+		
+	}
+	else if (_city->getOrganism( (_x - 1), _y ) == nullptr) {
+	
+		
+	}
+	else if (_city->getOrganism(_x, ( _y + 1 ) ) == nullptr) {
+	
+		
+	}
+	else if (_city->getOrganism(_x, ( _y - 1 ) ) == nullptr) {
+	
+		
+	}
 }
